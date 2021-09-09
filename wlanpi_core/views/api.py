376a2e9c18e8c8555e0b5c25c95ef988515d8000
Josh Schmelzle <josh@joshschmelzle.com>
2021-09-09 00:31:45 -0400

    consolidate api landing page view

--- wlanpi_core/views/api.py
+++ wlanpi_core/views/api.py
@@ -17,16 +17,11 @@ router = fastapi.APIRouter()
 
 
 @router.get("/", include_in_schema=False)
-async def index(request: Request):
-    data = {"request": request}
-    return templates.TemplateResponse("home/index.html", data)
-
-
 @router.get("/api", include_in_schema=False)
 @router.get("/api/v1", include_in_schema=False)
 async def index(request: Request):
     data = {"request": request, "endpoints": ENDPOINTS}
-    return templates.TemplateResponse("home/api.html", data)
+    return templates.TemplateResponse("home/index.html", data)
 
 
 @router.get("/favicon.ico", include_in_schema=False)
