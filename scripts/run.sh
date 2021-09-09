559cf408fed377c6a1de106cc022243d909270ec
Josh Schmelzle <josh@joshschmelzle.com>
2021-09-09 00:01:53 -0400

    fix run.sh script which could not find app

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,5 @@
-uvicorn wlanpi_core.main:app --reload --env-file .env  --port 8000 --host 0.0.0.0
+#!/usr/bin/env bash
+
+set -x
+
+uvicorn wlanpi_core.app:app --reload --env-file .env  --port 8000 --host 0.0.0.0
