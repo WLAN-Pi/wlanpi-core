3f1330f28d5f4542b7472115ac8760b562180d3a
Josh Schmelzle <josh@joshschmelzle.com>
2021-09-16 01:20:52 -0400

    format network_api

--- wlanpi_core/api/api_v1/endpoints/network_api.py
+++ wlanpi_core/api/api_v1/endpoints/network_api.py
@@ -53,17 +53,17 @@ async def retrieve_public_ip_information():
 @router.get("/localip")
 def get_local_ip():
     """
-    Return the determined primary local IP address. 
+    Return the determined primary local IP address.
 
     TODO: Test get_local_ip() when Pi has no connectivity. Abstract out to a service.
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         # does not have to be reachable
-        s.connect(('10.255.255.255', 1))
+        s.connect(("10.255.255.255", 1))
         IP = s.getsockname()[0]
     except Exception:
-        IP = '127.0.0.1'
+        IP = "127.0.0.1"
     finally:
         s.close()
-    return {'ip': IP}
+    return {"ip": IP}
