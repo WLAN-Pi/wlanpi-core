e05c4617d64865a2a7fb363ee2818562e0406f2e
Josh Schmelzle <josh@joshschmelzle.com>
2021-09-21 00:17:55 -0400

    isort doesn't need --recursive anymore

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,4 +4,4 @@ set -x
 
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place wlanpi_core --exclude=__init__.py
 black wlanpi_core
-isort wlanpi_core --recursive --diff
+isort wlanpi_core 
