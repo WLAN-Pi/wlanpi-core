19f43c8396672902f8d1bc3b728f3cb926bda742
Josh Schmelzle <josh@joshschmelzle.com>
2022-10-24 22:21:21 -0400

    use /docs rather than /documentation

--- wlanpi_core/app.py
+++ wlanpi_core/app.py
@@ -4,6 +4,7 @@
 import logging
 
 # third party imports
+import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.staticfiles import StaticFiles
@@ -14,8 +15,9 @@ from wlanpi_core.api.api_v1.api import api_router
 from wlanpi_core.core.config import endpoints, settings
 from wlanpi_core.views import api
 
-log = logging.getLogger("uvicorn")
-
+# setup logger
+log_config = uvicorn.config.LOGGING_CONFIG
+log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
 
 def create_app():
     app = FastAPI(
@@ -23,10 +25,11 @@ def create_app():
         description=settings.PROJECT_DESCRIPTION,
         version=__version__,
         license_info={"name": __license__, "url": __license_url__},
-        docs_url="/documentation",
+        docs_url="/docs",
         redoc_url=None,
         openapi_url=f"{settings.API_V1_STR}/openapi.json",
         openapi_tags=settings.TAGS_METADATA,
+        log_config=log_config,
     )
 
     app.include_router(api_router, prefix=settings.API_V1_STR)
