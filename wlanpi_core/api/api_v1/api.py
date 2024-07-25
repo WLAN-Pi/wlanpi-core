d76d9a160975996b45ad52f3ea3a43277700ec86
root <root@e3bb92b6e95c>
2024-07-25 23:41:37 +0100

    add utils files and endpoints + reachibility

--- wlanpi_core/api/api_v1/api.py
+++ wlanpi_core/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from wlanpi_core.api.api_v1.endpoints import network_api, system_api
+from wlanpi_core.api.api_v1.endpoints import network_api, system_api, utils_api
 
 api_router = APIRouter()
 
@@ -8,4 +8,8 @@ api_router.include_router(system_api.router, prefix="/system", tags=["system"])
 
 api_router.include_router(
     network_api.router, prefix="/network", tags=["network information"]
+)
+
+api_router.include_router(
+    utils_api.router, prefix="/utils", tags=["device utils"]
 )
