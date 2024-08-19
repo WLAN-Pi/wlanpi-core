40719591334ca72e990cdddc3eaf2a0ff2fff2a7
root <root@e3bb92b6e95c>
2024-08-19 13:13:20 +0100

    add bluetooth (pairing not implemented)

--- wlanpi_core/api/api_v1/api.py
+++ wlanpi_core/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from wlanpi_core.api.api_v1.endpoints import network_api, system_api, utils_api
+from wlanpi_core.api.api_v1.endpoints import network_api, system_api, utils_api, bluetooth_api
 
 api_router = APIRouter()
 
@@ -9,3 +9,5 @@ api_router.include_router(system_api.router, prefix="/system", tags=["system"])
 api_router.include_router(network_api.router, prefix="/network", tags=["network"])
 
 api_router.include_router(utils_api.router, prefix="/utils", tags=["device utils"])
+
+api_router.include_router(bluetooth_api.router, prefix="/bluetooth", tags=["bluetooth"])
---
+++ wlanpi_core/api/api_v1/endpoints/bluetooth_api.py
@@ -0,0 +1,66 @@
+import logging
+import os
+
+from fastapi import APIRouter, Response
+
+from wlanpi_core.models.validation_error import ValidationError
+from wlanpi_core.schemas import bluetooth
+from wlanpi_core.services import bluetooth_service
+
+import subprocess
+
+router = APIRouter()
+
+log = logging.getLogger("uvicorn")
+
+
+@router.get("/status", response_model=bluetooth.BluetoothStatus)
+async def btstatus():
+    """
+    Returns the bluetooth status
+    """
+
+    try:
+        status = bluetooth_service.bluetooth_status()
+        if status == False:
+            return Response(content=f"Bluetooth hardware not found", status_code=500)
+        return status
+        
+    except ValidationError as ve:
+        return Response(content=ve.error_msg, status_code=ve.status_code)
+    except Exception as ex:
+        log.error(ex)
+        return Response(content=f"Internal Server Error", status_code=500)
+    
+    
+@router.get("/power/{action}", response_model=bluetooth.PowerState)
+async def bt_power(action: str):
+    """
+    Turns on bluetooth
+    
+    - action: "on" or "off"
+    """
+    
+    # Validate action parameter
+    if action not in ["on", "off"]:
+        return Response(content="Invalid action. Use 'on' or 'off'.", status_code=400)
+    
+    # Convert action to Boolean
+    state = action == "on"
+    
+    if not bluetooth_service.bluetooth_present():
+        return Response(content=f"Bluetooth hardware not found", status_code=500)
+
+    try:
+        status = bluetooth_service.bluetooth_set_power(state)
+    
+        if status == False:
+            return Response(content=f"Bluetooth failed to turn {action}", status_code=500)
+    
+        return {"status": "success", "action": action}
+        
+    except ValidationError as ve:
+        return Response(content=ve.error_msg, status_code=ve.status_code)
+    except Exception as ex:
+        log.error(ex)
+        return Response(content=f"Internal Server Error {ex}", status_code=500)
