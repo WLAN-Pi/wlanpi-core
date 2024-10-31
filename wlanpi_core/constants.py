1a04478b34aaa32f56cec66b6e7b98f5d7622528
Dylan Toner <dylanjtoner@gmail.com>
2024-10-31 13:55:24 +0000

    fix formatting manually

--- wlanpi_core/constants.py
+++ wlanpi_core/constants.py
@@ -1,7 +1,9 @@
 # Core config
 API_V1_STR: str = "/api/v1"
 PROJECT_NAME: str = "wlanpi-core"
-PROJECT_DESCRIPTION: str = "The wlanpi-core API provides endpoints for applications on the WLAN Pi to share data. 🚀"
+PROJECT_DESCRIPTION: str = (
+    "The wlanpi-core API provides endpoints for applications on the WLAN Pi to share data. 🚀"
+)
 
 # Linux programs
 IFCONFIG_FILE: str = "/sbin/ifconfig"
