24a96e52b65a9ea4865e0fbdd8d1412279e78112
Josh Schmelzle <josh@joshschmelzle.com>
2025-01-14 20:50:35 -0600

    make sure shared secret can be read by webui

--- wlanpi_core/core/security.py
+++ wlanpi_core/core/security.py
@@ -1,3 +1,5 @@
+import grp
+import pwd
 import secrets
 from pathlib import Path
 from typing import Optional
@@ -47,9 +49,24 @@ class SecurityManager:
             if not secret_path.exists():
                 secret = secrets.token_bytes(32)
                 secret_path.write_bytes(secret)
+                # Set file ownership to root:wlanpi
+                uid = pwd.getpwnam('root').pw_uid
+                gid = grp.getgrnam('wlanpi').gr_gid
+                secret_path.chown(uid, gid)
+                # Set permissions to 0o640 - readable by owner (root) and group (wlanpi)
+                secret_path.chmod(0o640)
                 secret_path.chmod(0o600)
                 log.debug("Generated new shared secret")
             else:
+                stat = secret_path.stat()
+                uid = pwd.getpwnam('root').pw_uid
+                gid = grp.getgrnam('wlanpi').gr_gid
+                if stat.st_uid != uid or stat.st_gid != gid:
+                    secret_path.chown(uid, gid)
+                    log.debug("Updated secret file ownership to root:wlanpi")
+                if stat.st_mode & 0o777 != 0o640:
+                    secret_path.chmod(0o640)
+                    log.debug("Updated secret file permissions to 0o640")
                 secret = secret_path.read_bytes()
                 if not secret:
                     raise ValueError("Empty shared secret file")
